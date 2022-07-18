caafa7458c690c9e6d7016603055c5997f979248
mayurkanade44 <mayurkanade44@gmail.com>
2022-07-18 11:17:19 +0530

    initial setup client setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { ServiceRequest } from "./components/ServiceRequest";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center">CRM</h1>
+      <ServiceRequest />
+    </div>
+  );
+}
+
+export default App;

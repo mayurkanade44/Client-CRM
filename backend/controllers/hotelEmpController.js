9c6fcaac63e928852ca3b5fa042f2aca0f318650
mayurkanade44 <mayurkanade44@gmail.com>
2022-07-19 16:40:26 +0530

    hotel employee login id change to username

--- backend/controllers/hotelEmpController.js
+++ backend/controllers/hotelEmpController.js
@@ -1,17 +1,17 @@
 import HotelEmployee from "../models/HotelEmployee.js";
 
 export const employeeRegister = async (req, res) => {
-  const { name, email, department, password } = req.body;
+  const { name, username, department, password } = req.body;
 
   try {
-    if (!name || !department || !password || !email) {
+    if (!name || !department || !password || !username) {
       return res.status(400).json({ msg: "Please enter all values" });
     }
 
-    const alreadyExists = await HotelEmployee.findOne({ email });
+    const alreadyExists = await HotelEmployee.findOne({ username });
     if (alreadyExists) {
       return res.status(400).json({
-        msg: "Given email id already exists. Try different email id.",
+        msg: "Given username already exists. Try different username.",
       });
     }
 
@@ -26,28 +26,29 @@ export const employeeRegister = async (req, res) => {
 };
 
 export const employeeLogin = async (req, res) => {
-  const { email, password, hotel } = req.body;
+  const { username, password, hotel } = req.body;
 
   try {
-    if (!password || !email || !hotel) {
+    if (!password || !username || !hotel) {
       return res.status(400).json({ msg: "Please enter all values" });
     }
 
-    const user = await HotelEmployee.findOne({ email });
+    const user = await HotelEmployee.findOne({ username, hotel });
     if (!user) {
-      return res.status(400).json({ msg: "Invalid Email Id" });
+      return res.status(400).json({ msg: "Invalid username" });
     }
 
     const isPasswordMatch = await user.comparePassword(password);
     if (!isPasswordMatch) {
       return res.status(400).json({ msg: "Invalid password" });
     }
-    const userHotel = user.hotel.toString().replace(/ObjectId\("(.*)"\)/, "$1");
-    if (hotel !== userHotel) {
-      return res
-        .status(400)
-        .json({ msg: "User is not registered in the selected hotel" });
-    }
+    
+    // const userHotel = user.hotel.toString().replace(/ObjectId\("(.*)"\)/, "$1");
+    // if (hotel !== userHotel) {
+    //   return res
+    //     .status(400)
+    //     .json({ msg: "User is not registered in the selected hotel" });
+    // }
 
     const token = await user.createJWT();
     res.status(200).json({ token, msg: "logged in" });

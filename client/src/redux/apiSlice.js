b36a426ef793fd1455193c7b24b3905b745fad7b
mayurkanade44 <mayurkanade44@gmail.com>
2023-12-21 16:00:20 +0530

    unauthorized access bug fixed

--- client/src/redux/apiSlice.js
+++ client/src/redux/apiSlice.js
@@ -1,11 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { toast } from "react-toastify";
+import { removeCredentials } from "./helperSlice";
 
 const baseQuery = fetchBaseQuery({ baseUrl: "/" });
 const authBaseQuery = async (args, api, extraOption) => {
   const result = await baseQuery(args, api, extraOption);
   if (result.error && result.error.status === 401) {
     toast.error("Unauthorized!! logged out");
+    api.dispatch(removeCredentials());
     return;
   }
   return result;
